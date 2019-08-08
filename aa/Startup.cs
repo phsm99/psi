aab31a87e7dc1f06274c7be5648eedfa8cbe9cf7
1075305 <1075305@PSG10INFLB20711>
2019-08-07 21:35:26 -0300

    Add project files.

---
+++ aa/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(aa.Startup))]
+namespace aa
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
