6e62a41cafcd7796ac9d6843546067aa91f01170
1075305 <1075305@PSG10INFLB20529>
2019-08-12 20:02:55 -0300

    first commit

---
+++ aa/BD/AppInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using aa.Models;
+
+namespace aa.BD
+{
+    public class AppInitializer : DropCreateDatabaseIfModelChanges<AppContext>
+    {
+        protected override void Seed(AppContext context)
+        {
+            var Tar = new List<Tarefa> { new Tarefa { Titulo = "Primeira Tarefa" }, new Tarefa { Titulo = "Segunda Tarefa" } };
+            Tar.ForEach(s => context.Tarefas.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ aa/Models/Tarefa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aa.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string DataCriacao { get; set; }
+        
+    }
+}
