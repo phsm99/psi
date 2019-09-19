aaf0c254efddb5aa4efc85856d81a1269fd8152d
PEDRO CASA <PEDRO@DESKTOP-ISDNKR2>
2019-09-18 22:50:31 -0300

    pequenos ajustes no usuario model

--- aa/Models/Usuario.cs
+++ aa/Models/Usuario.cs
@@ -11,15 +11,20 @@ namespace aa.Models
             Lider
         }
         public int Id { get; set; }
+        [Required]
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public int Idade { get; set; }
+        [Required]
         public string CPF { get; set; }
+        [Required]
         [DataType(DataType.Password)]
         public string Senha { get; set; }
         [EmailAddress]
+        [Required]
         public string Email { get; set; }
         public virtual ICollection<Tarefa> Tarefas { get; set; }
+        [Required]
         public Tipo Cargo { get; set; }
     }
 }
